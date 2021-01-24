d0893acbe274a780e72f111f459cd68ff47870ca
张亚飞 <672233580@qq.com>
2021-01-24 20:20:16 +0800

    Initial Commit

---
+++ TopRefresh/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TopRefresh
+//
+//  Created by 张亚飞 on 2021/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TopRefresh/TopRefreshApp.swift
@@ -0,0 +1,17 @@
+//
+//  TopRefreshApp.swift
+//  TopRefresh
+//
+//  Created by 张亚飞 on 2021/1/24.
+//
+
+import SwiftUI
+
+@main
+struct TopRefreshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
